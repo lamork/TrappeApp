cb467099f8a89e0559a92f20bcc0ac99e4f362a6
lamork <leif.asmund.mork@gmail.com>
2015-12-20 13:04:23 +0100

    Sorting descending on leaderboard

--- app/controllers/leaderboard.js
+++ app/controllers/leaderboard.js
@@ -1,6 +1,38 @@
 import Ember from 'ember';
-import groupBy from 'ember-group-by';
+
+var A = Ember.A;
+var computed = Ember.computed;
+var get = Ember.get;
+var isPresent = Ember.isPresent;
 
 export default Ember.Controller.extend({
-  usersByDaysWalked: groupBy('model', 'user.name'),
+  usersByDaysWalked: groupBy('model', 'user.name')
 });
+
+export default function groupBy(collection, property) {
+  var dependentKey = collection + '.@each.' + property;
+
+  return computed(dependentKey, function() {
+    var groups = new A();
+    var items = get(this, collection);
+
+    items.forEach(function(item) {
+      var value = get(item, property);
+      var group = groups.findBy('value', value);
+
+      if (isPresent(group)) {
+        group.total += 1;
+      } else {
+        group = { property: property, value: value, total: 1 };
+        groups.push(group);
+      }
+    });
+
+    var sortedGroups = groups.sort(SortTotalHighToLow)
+    return sortedGroups;
+  }).readOnly();
+}
+
+function SortTotalHighToLow(a, b) {
+	return b.total-a.total;
+}
