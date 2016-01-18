8a383f62816a163788ead2c847640b575bbacc34
lamork <leif.asmund.mork@gmail.com>
2016-01-18 21:51:44 +0100

    Added bootstrap alert to history

--- app/controllers/registerwalkedstaircase.js
+++ app/controllers/registerwalkedstaircase.js
@@ -2,25 +2,25 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   actions: {
-      registerNewWalkedStaircase: function() {
-        var walkedstaircaseModel = this.get('model');
-        var user = this.get('session.currentUser');
+    registerNewWalkedStaircase: function() {
+      var walkedstaircaseModel = this.get('model');
+      var user = this.get('session.currentUser');
 
-        var newWalkedstaircase = this.store.createRecord('walkedstaircase', {
-          dateWalked: walkedstaircaseModel.dateWalked,
-          created: walkedstaircaseModel.created,
-          user: user
+      var newWalkedstaircase = this.store.createRecord('walkedstaircase', {
+        dateWalked: walkedstaircaseModel.dateWalked,
+        created: walkedstaircaseModel.created,
+        user: user
+      });
+
+      user.get('allWalkedStaircases').pushObject(newWalkedstaircase);
+
+      newWalkedstaircase.save().then(function() {
+        user.save();
+        $("#status span").text("Trappen er registrert")
+        .show().parent().fadeIn()
+        .delay(1000).fadeOut('slow', function() {
+          $("#registerStatus span").text('');
         });
-
-        user.get('allWalkedStaircases').pushObject(newWalkedstaircase);
-
-        newWalkedstaircase.save().then(function() {
-          user.save();
-          $("#registerStatus span").text("Trappen er registrert")
-            .show().parent().fadeIn()
-            .delay(2000).fadeOut('slow', function() {
-                $("#registerStatus span").text('');
-            });
-        });
+      });
     }
-}});
+  }});
--- app/controllers/viewwalkedstaircases.js
+++ app/controllers/viewwalkedstaircases.js
@@ -4,5 +4,11 @@ export default Ember.Controller.extend({
   actions: {remove: function(walkedstaircase){
     walkedstaircase.deleteRecord();
     walkedstaircase.save();
+
+    $("#status span").text("Oppføringen er slettet")
+    .show().parent().fadeIn()
+    .delay(1000).fadeOut('slow', function() {
+      $("#registerStatus span").text('');
+    });
   }
 }});
