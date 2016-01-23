4b85e9be2f814d500a221ad8870218937700c28d
lamork <leif.asmund.mork@gmail.com>
2016-01-23 17:14:10 +0100

    Added prod config

--- config/environment.js
+++ config/environment.js
@@ -3,14 +3,13 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'trappe-app',
-    environment: 'development',
+    environment: environment,
     baseURL: '/',
     locationType: 'auto',
     contentSecurityPolicy: {
       'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com",
       'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com",
       'script-src': "'self' 'unsafe-inline'", },
-    firebase: 'https://trappeapp-test.firebaseio.com',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -25,27 +24,11 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    //  ENV.APP.LOG_RESOLVER = true;
-    //  ENV.APP.LOG_ACTIVE_GENERATION = true;
-    //  ENV.APP.LOG_TRANSITIONS = true;
-    //  ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    //  ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
-
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
-
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
-
-    ENV.APP.rootElement = '#ember-testing';
+    ENV.firebase= 'https://trappeapp-test.firebaseio.com';
   }
 
   if (environment === 'production') {
-
+      ENV.firebase= 'https://trappeapp.firebaseio.com';
   }
 
   return ENV;
